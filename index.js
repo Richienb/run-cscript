017cf582b157af0f88a372a9325fc5345b7788d7
Richie Bendall <richiebendall@gmail.com>
2020-01-26 00:59:18 +1300

    feat: Update code
    Signed-off-by: Richie Bendall <richiebendall@gmail.com>

--- index.js
+++ index.js
@@ -1,7 +1,35 @@
 "use strict"
 
-module.exports = (input, { postfix = "rainbows" } = {}) => {
-	if (typeof input !== "string") throw new TypeError(`Expected a string, got ${typeof input}`)
+const execa = require("execa")
+const commandExists = require("command-exists")
+const fs = require("fs-extra")
+const { default: ow } = require("ow")
+const { default: is } = require("@sindresorhus/is")
 
-	return `${input} & ${postfix}`
+module.exports = async (input, { interactive = false, timeout = Infinity, host = "wscript", logo = false, unicode = true, cwd = process.cwd() } = {}) => {
+	if (process.platform !== "win32") throw new Error("Windows is required!")
+	if (!(await commandExists("cscript"))) throw new Error("`cscript` not found!")
+
+	ow(input, ow.string)
+	ow(interactive, ow.boolean)
+	ow(timeout, ow.number.integerOrInfinite)
+	ow(host, ow.string.matches(/cscript|wscript/))
+	ow(logo, ow.boolean)
+	ow(unicode, ow.boolean)
+	ow(cwd, ow.string)
+
+	const args = [input, interactive ? "//I" : "//B", host === "cscript" ? "//H:CScript" : "//H:WScript", logo ? "//Logo" : "//Nologo"]
+
+	if (is.number(timeout) && !is.infinite(timeout)) args.push(`//T:${timeout}`)
+	if (unicode) args.push("//U")
+
+	const options = {
+		cwd
+	}
+
+	if (is.number(timeout) && !is.infinite(timeout)) options.timeout = timeout
+
+	const { stdout } = await execa("cscript", args, options)
+
+	return stdout
 }
